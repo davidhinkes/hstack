fe56e861a318c3cc00525c45bc6a3059965ee9cc
David Hinkes <david.hinkes@gmail.com>
2013-02-12 04:06:54 +0000

    added example server

---
+++ srcs/Server.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Data.Serialize
+import Network.Hstack
+import Snap.Http.Server.Config
+import Snap.Http.Server
+import Network.Hstack.Example (d, h)
+
+main = do
+  httpServe defaultConfig (createSnap d h)
